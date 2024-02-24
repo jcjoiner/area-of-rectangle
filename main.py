fc23c9e6f150351e37c00b558e07928e12b789af
jcjoiner <jcjoiner555@gmail.com>
2024-02-24 12:00:56 -0600

    Simple calculator to find the area of a rectangle.

---
+++ main.py
@@ -0,0 +1,12 @@
+# Problem Set 1
+# Calculate Area of a Rectangle
+
+'''
+    Write a Python program that calculates the area of a rectangle given its length and its width.
+'''
+
+# Area of rectangle formula: area = length * width
+length = int(input("Enter your length: "))
+width = int(input("Enter your width: "))
+area = length * width
+print(f"The area of your rectangle is: {area}\N{SUPERSCRIPT TWO}")
